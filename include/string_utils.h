f27cdfeade743c310dbc0e8bcaa39373a5741426
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-01-21 14:54:45 -0700

    Goodbye spaghetti code in char_to_int, hello efficient beauty!
    Oh, by the way, I don't have a linux build environment set up, so I suggest you make test before pushing it into your repository.

--- include/string_utils.h
+++ include/string_utils.h
@@ -39,31 +39,10 @@ namespace qgl
     inline
     int char_to_int(char c)
     {
-        switch (c)
-        {
-            case '0':
-                return 0;
-            case '1':
-                return 1;
-            case '2':
-                return 2;
-            case '3':
-                return 3;
-            case '4':
-                return 4;
-            case '5':
-                return 5;
-            case '6':
-                return 6;
-            case '7':
-                return 7;
-            case '8':
-                return 8;
-            case '9':
-                return 9;
-            default:
-                throw std::logic_error("not a number");
-        }
+        if((c < '0') || (c > '9'))
+            throw std::logic_error("not a number");
+
+        return c - '0';
     }
     
     template <typename T0, typename T1, typename T2, typename T3, typename T4, 
